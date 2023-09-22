b81f09eb998f6323d83d9df8c5b1441541a548d7
MalcolmPete <58274443+MalcolmPete@users.noreply.github.com>
2023-09-22 11:28:47 -0400

    Update leagueInfo.js
    fixed tai favorite team

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -149,7 +149,7 @@ export const managers = [
       "bio": "Knows something we don't.",
       "photo": "/managers/tai.jpeg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2018, // (optional) when did the manager start playing fantasy football
-      "favoriteTeam": "nos", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "favoriteTeam": "no", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Trevor", // Can be anything (usually your rival's name)
