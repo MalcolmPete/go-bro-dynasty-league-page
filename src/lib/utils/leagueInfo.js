439d1e6df1a2cfd57e7d3da46dc78d6497ade1c5
MalcolmPete <58274443+MalcolmPete@users.noreply.github.com>
2023-09-22 11:26:55 -0400

    Update leagueInfo.js
    fixed trevor and jake pics

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -32,7 +32,7 @@ export const managers = [
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Metro", // (optional)
       "bio": "The self-proclaimed Trade Dealer.",
-      "photo": "/managers/trevor.jpeg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/trevor.png", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2018, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "sea", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
@@ -85,7 +85,7 @@ export const managers = [
       "rival": {
         name: "Jake", // Can be anything (usually your rival's name)
         link: 6, // manager array number within this array, or null to link back to all managers page
-        image: "/managers/jake.jpeg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+        image: "/managers/jake.png", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
       "favoritePlayer": 4988, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "valuePosition": "QB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
@@ -108,7 +108,7 @@ export const managers = [
       "rival": {
         name: "Trevor", // Can be anything (usually your rival's name)
         link: 1, // manager array number within this array, or null to link back to all managers page
-        image: "/managers/trevor.jpeg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+        image: "/managers/trevor.png", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
       "favoritePlayer": 4984, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "valuePosition": "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
@@ -154,7 +154,7 @@ export const managers = [
       "rival": {
         name: "Trevor", // Can be anything (usually your rival's name)
         link: 1, // manager array number within this array, or null to link back to all managers page
-        image: "/managers/trevor.jpeg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+        image: "/managers/trevor.png", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
       "favoritePlayer": 4035, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
@@ -193,7 +193,7 @@ export const managers = [
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Ottawa", // (optional)
       "bio": "Cornerback for the Carleton Ravens.",
-      "photo": "/managers/jake.jpeg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/jake.png", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2018, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "chi", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
